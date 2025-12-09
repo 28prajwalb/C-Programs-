7e09b4347c5c745bc06a4887b25c4c0aac89aca4
28prajwalb <bp6208537@gmail.com>
2025-12-09 18:48:21 +0530

    prg2.c

---
+++ prg2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter the value of a\n");
+    scanf("%d",&a);
+    printf("enter the value of b\n");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("The Sum of a and b is %d",sum);
+}
+
+output: enter the value of a
+10
+enter the value of b
+10
+The Sum of a and b is 20
